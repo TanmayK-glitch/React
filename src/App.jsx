26952c538fa8408accb8b8a7f40331a7f475a8e7
Tanmay <tanmaykangule7271@gmail.com>
2025-09-18 14:42:44 +0530

    Learned Rendering Lists

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
 import Lists from "./Lists"
 
-function App(){
-  return(<Lists />);
+function App() {
+
+  const fruits = [{ id: 1, name: "Apple", calories: 90 },
+  { id: 2, name: "Orange", calories: 120 },
+  { id: 3, name: "Pineapple", calories: 200 },
+  { id: 4, name: "Cherry", calories: 150 }];
+
+  const vegetables = [{ id: 5, name: "Carrot", calories: 23 },
+    { id: 6, name: "Tomaotes", calories: 44 },
+    { id: 7, name: "Ladysfinger", calories: 99 },
+    { id: 8, name: "Onion", calories: 12 }];
+
+
+  return (
+    <>
+      {fruits.length > 0 && <Lists item = {fruits} category = "Fruits"/>}
+      {vegetables.length > 0 && <Lists item = {vegetables} category = "Vegetables" />}
+    </>
+  );
 }
 
 export default App
--- src/Lists.jsx
+++ src/Lists.jsx
@@ -1,9 +1,23 @@
-function Lists(){
-    const fruits = ["Banana", "Apple", "Orange", "Pineapple", "Kiwi"];
+function Lists(props) {
 
-    const listItems = fruits.map(fruit => <li>{fruit}</li>);
+    // fruits.sort((a, b) => a.name.localeCompare(b.name)); // Alphabetically
+    // fruits.sort((a, b) => b.name.localeCompare(a.name)); // Reverse Alphabetically
+    // fruits.sort((a, b) => a.calories - b.calories); // Ascending 
+    // fruits.sort((a, b) => b.calories - a.calories); // Descending
 
-    return(<ol>{listItems}</ol>);
+    const itemList = props.item;
+
+    const newList = itemList.map(entry => <li key={entry.id}>{entry.name} - {entry.calories}</li>);
+
+    return (<>
+        <h3>{props.category}</h3>
+        <ol>{newList}</ol>
+    </>);
+}
+
+Lists.defaultProps = {
+    item: [],
+    category: "Category",
 }
 
 export default Lists
